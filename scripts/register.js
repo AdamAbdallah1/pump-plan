41c0a05a2e62f20f2243fe2099e80ae61ab20efd
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-05-01 17:33:25 +0300

    Adding configuration for firebase

---
+++ scripts/register.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyAqHl8T_S2YWhGNuWtjFV-7GE8Zqnmhj28",
+
+  authDomain: "pump-plan.firebaseapp.com",
+
+  projectId: "pump-plan",
+
+  storageBucket: "pump-plan.firebasestorage.app",
+
+  messagingSenderId: "745917737640",
+
+  appId: "1:745917737640:web:ef41fd0ef723eb2cf5309d"
+
+};
+
+const app = initializeApp(firebaseConfig);
