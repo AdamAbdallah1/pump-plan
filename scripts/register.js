0a48dd3460f7bee3e657db22f1f9feb2963cee70
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-05-01 17:51:44 +0300

    Setting firebase auth registeration email and password

--- scripts/register.js
+++ scripts/register.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
 
 const firebaseConfig = {
 
@@ -23,7 +24,18 @@ document.addEventListener("DOMContentLoaded", () => {
         const email = document.getElementById("email").value;
         const password = document.getElementById("password").value;
         if (email && password){
-            alert("Done");
+            const auth = getAuth();
+            createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                alert("Done user");
+                const user = userCredential.user;
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                alert(`Error: ${error.message}`);
+            });
+                
         } else {
             alert("no");
         }
