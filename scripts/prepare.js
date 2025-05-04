d76b780693fcd84ea2e7267c53a4e47fe954ac3d
Adam Abdallah <adamabdallahayln1@gmail.com>
2025-05-04 14:59:47 +0300

    Checking if fields are filled

--- scripts/prepare.js
+++ scripts/prepare.js
@@ -4,6 +4,13 @@ document.addEventListener("DOMContentLoaded", () => {
         const planSelect = document.getElementById("plan-select").value;
         const levelSelect = document.getElementById("level-select").value;
 
-        console.log(planSelect, levelSelect);
+        const resultsDisplay = document.getElementById("results");
+        if (planSelect && levelSelect) {
+            resultsDisplay.textContent = "Preparing your Plan";
+            resultsDisplay.style.color = "green";
+        } else {
+            resultsDisplay.textContent = "Please Select all Required Fields.";
+            resultsDisplay.style.color = "#810000";
+        }
     })
 })
